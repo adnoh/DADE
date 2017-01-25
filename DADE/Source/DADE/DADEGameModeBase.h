48ab911b14a9e517fecff5a045ab555e7fd356c6
Neriel <nerielnexus@gmail.com>
2017-01-25 14:17:45 +0900

    빈 프로젝트 추가
    Signed-off-by: Neriel <nerielnexus@gmail.com>

---
+++ DADE/Source/DADE/DADEGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "DADEGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DADE_API ADADEGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
