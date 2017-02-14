0e16f849e4fe11da4cbbc5960107b9a475a769fc
Neriel <nerielnexus@gmail.com>
2017-02-14 19:22:21 +0900

    Signed-off-by: Neriel <nerielnexus@gmail.com>

--- DADE/Source/DADE/DADE.Build.cs
+++ DADE/Source/DADE/DADE.Build.cs
@@ -10,12 +10,15 @@ public class DADE : ModuleRules
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+        // Uncomment if you are using Slate UI
+        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+        // Uncomment if you are using online features
+        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+
+        PrivateDependencyModuleNames.AddRange(new string[] { "Swipe" });
+        PrivateIncludePathModuleNames.AddRange(new string[] { "Swipe" });
 	}
 }
